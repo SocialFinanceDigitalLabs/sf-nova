55cc1e4692a925979414a2883f2cdf2bfd71ae3e
Francisco Macedo <54102462+franciscobmacedo@users.noreply.github.com>
2023-09-14 15:46:23 +0100

    update example app

--- examples/hello_app/app.py
+++ examples/hello_app/app.py
@@ -1,48 +1,6 @@
-# Copyright 2018-2022 Streamlit Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
 
-LOGGER = get_logger(__name__)
+name = st.text_input("What's your name?")
 
-
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
-
-    st.write("# Welcome to SF-nova! 👋")
-
-    st.markdown(
-        """
-        SF-nova uses Streamlit to build apps that run entirely in the browser. Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
-
-
-if __name__ == "__main__":
-    run()
+if name:
+    st.write(f'Hello {name}!')
